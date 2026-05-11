cd0317a5d7962a7a937402611c73d21af3d94c24
Aratz006 <ara_hue_flo@goierrieskola.org>
2026-05-11 15:44:09 +0200

    Update Conn.java

--- src/main/java/Util/Conn.java
+++ src/main/java/Util/Conn.java
@@ -8,7 +8,7 @@ public class Conn {
 
     private static final String URL = "jdbc:mysql://192.168.10.5:3306/erronka2?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
     private static final String USER = "root";
-    private static final String PASSWORD = "1MG2024";
+    private static final String PASSWORD = "2MG2024";
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
 
     static {
